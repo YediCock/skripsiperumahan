4bd4f4a8b9a0272139ce8ee651ac83a39779b573
fadhlihilmans <fadhli.hilmanss@gmail.com>
2026-06-05 19:49:35 +0700

    fix list user

--- resources/views/livewire/admin/list-user/edit.blade.php
+++ resources/views/livewire/admin/list-user/edit.blade.php
@@ -14,7 +14,7 @@
         
                         <nav class="breadcrumb-style-one" aria-label="breadcrumb">
                             <ol class="breadcrumb">
-                                <li class="breadcrumb-item "><a href="{{ route('listAdmin') }}" wire:navigate>List Admin</a></li>
+                                <li class="breadcrumb-item "><a href="{{ route('listUser') }}" wire:navigate>List Admin</a></li>
                                 <li class="breadcrumb-item active" aria-current="page">Edit list admin {{ $admin->name }}</li>
                             </ol>
                         </nav>
