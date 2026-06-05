935d6e36ac791b20322dd78705685de7f76ffecc
fadhlihilmans <fadhli.hilmanss@gmail.com>
2026-06-05 19:42:55 +0700

    logo

--- resources/views/livewire/user/auth/logout.blade.php
+++ resources/views/livewire/user/auth/logout.blade.php
@@ -12,7 +12,7 @@
                 class="absolute top-6 h-[2px] w-0 bg-gradient-to-r from-primary to-primary-400 transition-all duration-500 group-hover:w-[40px]">
             </div>
         </a>
-        <a class="group relative" href="{{ route('register') }}">Register
+        <a class="group relative ml-4" href="{{ route('register') }}">Register
             <div aria-hidden="true"
                 class="absolute top-6 h-[2px] w-0 bg-gradient-to-r from-primary to-primary-400 transition-all duration-500 group-hover:w-[40px]">
             </div>
